5410c534bc8c3811df3723a32b2c519b49b03e89
Peter Bunyan <pete@blueshed.co.uk>
2012-11-19 20:31:37 +0000

    added a setup.py for install

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='for_james',
+      version=version,
+      description="samples in tornado",
+      long_description="""a hello world in tornado""",
+      author='Peter Bunyan',
+      author_email='pete@blueshed.co.uk',
+      url='http://www.blueshed.co.uk/for_james',
+      packages=find_packages('src',exclude=['*tests*']),
+      package_dir = {'':'src'},
+      include_package_data = True, 
+      exclude_package_data = { '': ['tests/*'] },
+      install_requires = [
+        'setuptools',
+        'tornado>=2.4',
+        'sqlalchemy>=0.7.9'
+      ],
+      entry_points = {
+      'console_scripts' : [
+                           'publish_for_james = simple_publish.server:main'
+                           ]
+      })
